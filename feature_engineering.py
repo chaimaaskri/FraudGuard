7b9296792fc2d8371e0ee392e1282ada2b77d6fa
chaima <chaima.askri@esprit.tn>
2025-02-24 12:24:13 +0100

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,15 +1,18 @@
 import pandas as pd
 
-def feature_engineering(data):
-    """
-    Generate additional features for the model.
-    """
-    # Convert 'TransactionTime' to datetime object
-    data['TransactionTime'] = pd.to_datetime(data['TransactionTime'])
+def process_features(data):
+    # Remove unrecognized timezone (IST, UTC, etc.)
+    data['TransactionTime'] = data['TransactionTime'].str.replace(r' [A-Z]{3,4} ', ' ', regex=True)
+    
+    # Convert to datetime
+    data['TransactionTime'] = pd.to_datetime(data['TransactionTime'], errors='coerce')
 
-    # Extract day of week and hour from the TransactionTime
+    # Extract features
     data['DayOfWeek'] = data['TransactionTime'].dt.dayofweek
     data['Hour'] = data['TransactionTime'].dt.hour
 
-    # You can add more features if necessary
+    # Drop rows where 'TransactionTime' is NaT
+    data = data.dropna(subset=['TransactionTime'])
+
     return data
+
